7b19c61e594044bde0e899854e7d9fbe6112560a
Eliza Gancarz <elizaganca@gmail.com>
2023-10-04 01:35:42 +0200

    - add drink edit view - move dashboard forms to cocktails forms

--- drinks/dashboard/urls.py
+++ drinks/dashboard/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
-    path('create/', views.create, name='create'),
+    path('create/', views.create_drink, name='create'),
+    path('edit/<int:drink_id>/', views.edit_drink, name='edit'),
     path('delete_drink/<int:drink_id>/', views.delete_drink, name='delete_drink'),
-
 ]
--- drinks/dashboard/views.py
+++ drinks/dashboard/views.py
@@ -7,8 +7,8 @@ from django.db.models import Q
 from django.http import HttpResponseNotAllowed
 from django.shortcuts import render, redirect, get_object_or_404
 
+from cocktails.forms import DrinkForm, IngredientFormset
 from cocktails.models import Drink
-from .forms import DrinkForm, IngredientFormset
 
 
 @login_required()
@@ -35,7 +35,7 @@ def dashboard(request):
 
 
 @login_required()
-def create(request):
+def create_drink(request):
     if request.method == 'GET':
         return render(request, 'create.html', {'drink_form': DrinkForm(), 'ingredient_formset': IngredientFormset()})
     elif request.method == 'POST':
@@ -52,6 +52,35 @@ def create(request):
                 return redirect('dashboard')
         messages.error(request, 'Popraw poniższe błędy:')
         return render(request, 'create.html', {'drink_form': drink_form, 'ingredient_formset': ingredients_formset})
+    else:
+        return HttpResponseNotAllowed(permitted_methods=['GET', 'POST'])
+
+
+@login_required
+def edit_drink(request, drink_id):
+    drink = get_object_or_404(Drink, pk=drink_id, owner=request.user)
+
+    if request.method == 'GET':
+        drink_form = DrinkForm(instance=drink)
+        ingredient_formset = IngredientFormset(instance=drink)
+        return render(request, 'create.html',
+                      {'drink_form': drink_form, 'ingredient_formset': ingredient_formset, 'drink': drink})
+
+    elif request.method == 'POST':
+        drink_form = DrinkForm(request.POST, request.FILES, instance=drink)
+        ingredient_formset = IngredientFormset(data=request.POST, instance=drink)
+        if drink_form.is_valid() and ingredient_formset.is_valid():
+            drink_form.save()
+            ingredient_formset.save()
+            form_ingredients = [form.instance for form in ingredient_formset]
+            for ingredient in drink.ingredients.all():
+                if ingredient not in form_ingredients:
+                    ingredient.delete()
+            messages.success(request, 'Drink został zaktualizowany')
+            return redirect('dashboard')
+        messages.error(request, 'Popraw poniższe błędy:')
+        return render(request, 'create.html',
+                      {'drink_form': drink_form, 'ingredient_formset': ingredient_formset, 'drink': drink})
     else:
         return HttpResponseNotAllowed(permitted_methods=['GET', 'POST'])
 
